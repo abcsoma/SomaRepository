2ad766f0b0407372d97340f6048a8f9b1e42b904
abcsoma <1062741978@qq.com>
2021-05-21 15:43:33 +0800

    项目初始化结构

---
+++ mall-coupon/src/main/java/com/soma/mall/mallcoupon/MallCouponApplication.java
@@ -0,0 +1,13 @@
+package com.soma.mall.mallcoupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallCouponApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MallCouponApplication.class, args);
+	}
+
+}
